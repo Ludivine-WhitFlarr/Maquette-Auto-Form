2026f00d1eaf268d9194e86fa5bdd87c9362522a
Ludivine <35698784+Ludivine-WhitFlarr@users.noreply.github.com>
2018-09-27 17:07:40 +0200

    fond jpg

---
+++ src/js/404.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Erreur extends Component {
+  render() {
+    return (
+          <div>
+            <p>Erreur 404</p>
+          </div>
+    );
+  }
+}
+
+export default Erreur;
